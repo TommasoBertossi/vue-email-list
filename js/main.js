200b4bdc7bd5d469f85eafae6d2cfd1c2b41fb9f
TommasoBertossi <tommasobertossi@gmail.com>
2023-09-07 09:57:05 +0200

    correction bug

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (response) {
-        const result = response.data
-        console.log(result);
-    });
+// axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then(function (response) {
+//         const result = response.data
+//         console.log(result);
+//     });
 
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      emails: [],
+      numberEmails: 10,
+    };
+  },
+
+  methods: {
+    fetchEmails() {
+
+        for (let i = 0; i < this.numberEmails; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            const generatedEmail = response.data.response;
+            this.emails.push(generatedEmail);
+        });
     }
-  }
-}).mount('#app')
+    },
+  },
+  mounted() {
+    this.fetchEmails();
+  },
+}).mount('#app');
